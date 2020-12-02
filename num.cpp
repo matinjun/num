684984d5cc9b379476199455fa8d07ed73e4f9e1
matinjun <44500594+matinjun@users.noreply.github.com>
2020-12-02 16:24:22 +0800

    第一次提交

---
+++ num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+	int a0 = 0;
+	int a1 = 1;
+	int tmp;
+	std::cout << "Hello git!" << std::endl;
+	std::cout << "--------------------test git----------------------" << std::endl;
+	std::cout << "Fibonacci sequence: " ;
+	std::cout << a1 << " ";
+	for(int i = 0; i < 10; ++i) {
+		tmp = a1;
+		a1 = a0 + a1;
+		std::cout << a1 << " ";
+		a0 = tmp;
+	}
+	std::cout << "...more\n" ;
+	return 0;
+}
+
